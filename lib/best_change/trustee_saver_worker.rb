51636d5369dfce6cedcd0ce54ba3a06312df7623
Roman Tershak <roman0tershak@gmail.com>
2024-07-29 23:43:50 +0300

    add usdt

--- lib/best_change/trustee_saver_worker.rb
+++ lib/best_change/trustee_saver_worker.rb
@@ -7,14 +7,20 @@ module BestChange
     sidekiq_options queue: :critical, retry: 3
 
     BASE_URL = 'https://api.v3.trustee.deals/data/all'
+    TOKEN_NETWORK_TO_CURRENCY = {
+      'trc20' => 'TRX_USDT',
+      'erc20' => 'ETH_USDT'
+    }.freeze
 
     def perform(exchange_rate_id, timestamp)
       exchange_rate = Gera::ExchangeRate.find(exchange_rate_id)
       ps1 = exchange_rate.income_payment_system.bestchange_id
       ps2 = exchange_rate.outcome_payment_system.bestchange_id
       key = BestChange::Repository.generate_key_from_bestchange_ids ps1, ps2, 'trustee'
+      in_currency = format_currency(exchange_rate.income_payment_system)
+      out_currency = format_currency(exchange_rate.outcome_payment_system)
 
-      url = "#{BASE_URL}?inCurrencyCode=#{exchange_rate.income_payment_system.currency}&outCurrencyCode=#{exchange_rate.outcome_payment_system.currency}"
+      url = "#{BASE_URL}?inCurrencyCode=#{in_currency}&outCurrencyCode=#{out_currency}"
       uri = URI(url)
       response = Net::HTTP.get(uri)
       data = JSON.parse(response)
@@ -30,6 +36,15 @@ module BestChange
       end
 
       BestChange::Repository.setRows key, rates.sort
+    end
+
+    private
+
+    def format_currency(payment_system)
+      currency = payment_system.currency.to_s.downcase
+      return unless currency.inquiry.usdt?
+
+      TOKEN_NETWORK_TO_CURRENCY[payment_system.token_network]
     end
   end
 end
