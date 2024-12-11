10645af6b16da4b07b610a1ee0d79e8b679bd365
Roman Tershak <roman0tershak@gmail.com>
2024-12-11 10:24:03 +0200

    Доработки LoadingWorker #2

--- lib/best_change/loading_worker.rb
+++ lib/best_change/loading_worker.rb
@@ -55,7 +55,9 @@ module BestChange
 
       logger.info "Finish (total benchmark: #{total_bm.real})\n"
 
-      exchange_rate_ids = Gera::TargetAutorateSetting.where('updated_at >= ?', 2.minutes.ago).pluck(:exchange_rate_id)
+      exchange_rate_ids1 = Gera::TargetAutorateSetting.where('updated_at >= ?', 2.minutes.ago).pluck(:exchange_rate_id)
+      exchange_rate_ids2 = Gera::ExchangeRate.where('updated_at >= ?', 2.minutes.ago).pluck(:id)
+      exchange_rate_ids = (exchange_rate_ids1 + exchange_rate_ids2)
       Gera::DirectionRateSnapshot.last.direction_rates.where(exchange_rate_id: exchange_rate_ids).each do |dr|
         dr.calculate_rate
         dr.save!
