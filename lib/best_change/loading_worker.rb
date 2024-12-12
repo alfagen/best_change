57f2ea3e71ed60da88371b77614ad7b5a2ce592b
Roman Tershak <roman0tershak@gmail.com>
2024-12-12 12:07:23 +0200

    f

--- lib/best_change/loading_worker.rb
+++ lib/best_change/loading_worker.rb
@@ -13,7 +13,6 @@ module BestChange
 
       logger.info output
       if $?.success?
-        logger.info 'DONE'
         exchange_rate_ids1 = Gera::TargetAutorateSetting.where('updated_at >= ?', 2.minutes.ago).pluck(:exchange_rate_id)
         exchange_rate_ids2 = Gera::ExchangeRate.where('updated_at >= ?', 2.minutes.ago).pluck(:id)
         exchange_rate_ids = (exchange_rate_ids1 + exchange_rate_ids2)
