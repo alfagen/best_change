e6be145bfba29635da7d7fee337722162e19df07
Roman Tershak <roman0tershak@gmail.com>
2025-10-16 19:46:08 +0300

    f

--- lib/best_change/repository.rb
+++ lib/best_change/repository.rb
@@ -8,6 +8,7 @@ require_relative 'row'
 module BestChange
   class Repository < RedisRepository
     KEY_SEPARATOR = '-'.freeze
+    CACHE_KEY = 'bestchange:preloaded_data:v1'.freeze
 
     class << self
       delegate :getRows, :setRows, :getRowByExchangerId, to: :instance
@@ -22,9 +23,7 @@ module BestChange
     # где ID1, ID2 - идентификаторы направления обмена из BestExchange (PaymentSystem#bestchange_id)
     # возвращает список BestChange::Row
     def getRows(key)
-      value = get key
-      return [] unless value.present?
-      Oj.load(value).each_with_index { |row, index| row.position = index }
+      preloaded_data[key]
     end
 
     def setRows(key, data)
@@ -36,6 +35,32 @@ module BestChange
       exchanger_id ||= BestChange.configuration.exchanger_id
 
       getRows(key).find { |row| row.exchanger_id == exchanger_id}
+    end
+
+    def preloaded_data
+      Rails.cache.fetch('bestchange_preloaded_data', expires_in: 30.seconds) do
+        cached = Redis.current.get(CACHE_KEY)
+        return Oj.load(cached) if cached.present?
+      
+        keys = Gera::ExchangeRate.all.map do |er|
+          er.bestchange_key
+        rescue Gera::CurrencyRatesRepository::UnknownPair, Gera::DirectionRate::UnknownExchangeRate, ActiveRecord::RecordInvalid
+          nil
+        end.compact
+
+        store = Repository.send(:new).send(:store)
+        raw_values = store.mget(*keys)
+        rows_cache = {}
+
+        keys.zip(raw_values).each do |key, json|
+          next unless json.present?
+          rows_cache[key] = Oj.load(json).each_with_index { |row, i| row.position = i }
+        end
+
+        Redis.current.set(CACHE_KEY, Oj.dump(rows_cache), ex: 30)
+
+        rows_cache
+      end
     end
   end
 end
