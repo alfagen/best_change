cbdd9b72ded0c0279d82631a8959a9d4dbecdce1
Roman Tershak <roman0tershak@gmail.com>
2025-10-20 18:29:11 +0300

    f

--- lib/best_change/row_serializer.rb
+++ lib/best_change/row_serializer.rb
@@ -2,7 +2,29 @@ class BestChange::RowSerializer
   include FastJsonapi::ObjectSerializer
   set_type :bestchange_row
 
-  attributes :exchanger_id, :exchanger_name, :buy_price, :sell_price, :reserve,
-    :rate, :base_rate_percent, :position
-  attributes :status
+  attributes :exchanger_id, :exchanger_name, :status
+
+  attribute :buy_price do |row|
+    row.buy_price.to_f
+  end
+
+  attribute :sell_price do |row|
+    row.sell_price.to_f
+  end
+
+  attribute :reserve do |row|
+    row.reserve.to_f
+  end
+
+  attribute :rate do |row|
+    row.rate.to_f
+  end
+
+  attribute :base_rate_percent do |row|
+    row.base_rate_percent.to_f
+  end
+
+  attribute :position do |row|
+    row.position.to_i
+  end
 end
