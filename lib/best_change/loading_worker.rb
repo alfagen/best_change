eb66443268fbfbeb0324724b1f35d72e85f5d213
Roman Tershak <roman0tershak@gmail.com>
2025-11-12 17:30:32 +0200

    until_executing -> while_executing

--- lib/best_change/loading_worker.rb
+++ lib/best_change/loading_worker.rb
@@ -5,7 +5,7 @@ module BestChange
     include ::Sidekiq::Worker
     include ::AutoLogger
 
-    sidekiq_options queue: :critical, retry: false, lock: :until_executed
+    sidekiq_options queue: :critical, retry: false, lock: :while_executing
 
     def perform
       bm = Benchmark.measure do
@@ -28,14 +28,6 @@ module BestChange
         end
 
         sleep 2
-
-        exchange_rate_ids1 = Gera::TargetAutorateSetting.where('updated_at >= ?', 30.seconds.ago).pluck(:exchange_rate_id)
-        exchange_rate_ids2 = Gera::ExchangeRate.where('updated_at >= ?', 30.seconds.ago).pluck(:id)
-        exchange_rate_ids = (exchange_rate_ids1 + exchange_rate_ids2).uniq
-        Gera::DirectionRateSnapshot.last.direction_rates.where(exchange_rate_id: exchange_rate_ids).each do |dr|
-          dr.calculate_rate
-          dr.save!
-        end
 
         BestChangeRatesExportWorker.perform_async
         GenerateCompositeStatusWorker.perform_async
